ce731eb7759a1a2764fb97424dbf47d30cd7b9a0
ealaurel <60707980+ealaurel@users.noreply.github.com>
2020-04-14 12:12:09 -0400

    Create e2_prac1_virtual.R

---
+++ codigos/e2_prac1_virtual.R
@@ -0,0 +1,47 @@
+#2 crear una funcion que calcule la mediana 
+#para tablas de fercuenciaa con intervalos de clases
+rm(list = ls())
+x<-matrix(c(13,15,17,19,21,15,17,19,21,23,4,9,3,3,1),5,3)
+x
+y<-as.data.frame(x)
+y
+names(y)[c(1,2,3)]<-c("Li","Ls","f")
+mediana<-function(tf,q){
+  #k:numero de intervalos
+  #n:frecuencia absoluta total
+  #c: ancho de clase del intervalo 
+  #Linf: limite inferior i_esimo
+  #acumF: fecuencia acumulada anterior al intervalo 
+  #fi: fec absoluta del intervalo i-esimo
+  #tf:tabla de fecuencias
+  #v:valor delcuntil a encontrar
+  k<-nrow(y)
+  n<-sum(tf[,3])
+  #q<-q/100
+  x<-n/2
+  #frecuencia acumulada aux
+  for (i in 1:k) {
+    F[i]<-sum(tf[1:i,3])
+    
+  }
+  
+  i=1#contador del intervalo buscado
+  while (F[i]<=x) {
+    i=i+1
+    
+  }
+  
+  Linf<-tf[i,1]
+  if(i==1){
+    acumF<-0}
+  else{
+    acumF<-F[i-1]}
+  fi<-tf[i,3]
+  c<-tf[i,2]-tf[i,1]
+  
+  v<-Linf+(x-acumF)*c/fi
+  return(v)
+}
+z<-mediana(y)
+z
+##
