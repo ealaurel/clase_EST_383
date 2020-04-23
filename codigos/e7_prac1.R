6eb8a96de8b5dbc52e4d65a4d720a505c74f6b83
ealaurel <60707980+ealaurel@users.noreply.github.com>
2020-04-23 13:34:31 -0400

    Create e7_prac1.R

---
+++ codigos/e7_prac1.R
@@ -0,0 +1,38 @@
+#7 utilizando la base de datos COVID 19 genere un grafico de evoluvion de contagios , muertes,
+#. y recuperados use los graficos de origen del r
+bdc_22abr<-read.csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",sep = ",")
+sum(bdc_22abr$X1.22.20)
+sum(bdc_22abr$X1.23.20)
+i=5
+j=1
+aux=c(1:92)
+while (i<=96) {
+  aux[j]=sum(bdc_22abr[,i])
+  i=i+1
+  j=j+1
+} 
+barplot(aux)
+?barplot
+?plot
+
+names(bdc_22abr)
+fechas<-c(names(bdc_22abr)[5:96])
+fechas
+
+names(aux)<-fechas
+names(aux)
+aux
+
+plot(aux,type="l",col="red",xlab = "desde el 22 ene al 22 abr",
+     ylab = "numero de infectados",main = "numero de infectados acumlados ")
+#numero de infectados por dia desde el 22ene al 22 abr
+i=6
+j=2
+aux2<-NULL
+while (i<=96) {
+  aux2[j]=sum(bdc_22abr[,i]-sum(bdc_22abr[,i-1])
+  i=i+1
+  j=j+1
+} 
+g=3
+g
